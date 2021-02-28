61650c4d76b9fdfd96b6d25dafdb52fce1c5bb05
NikoBolt <docnikbolt@gmail.com>
2021-03-01 00:35:40 +0300

    Stage 3/5: Tickets

--- Cinema Room Manager/task/src/cinema/Cinema.kt
+++ Cinema Room Manager/task/src/cinema/Cinema.kt
@@ -2,8 +2,107 @@ package cinema
 
 import java.util.*
 /*
-Stage 2/5: Sold!
+Stage 3/5: Tickets
 */
+fun main() {
+    val cinema = initCinema()
+    cinema.printScreenRoom()
+    cinema.sellTicket()
+    cinema.calculatePrice()
+    cinema.printScreenRoom()
+}
+
+fun initCinema(): Cinema {
+    println("Enter the number of rows:")
+    val scanner = Scanner(System.`in`)
+    val rows = scanner.nextInt()
+    println("Enter the number of seats in each row:")
+    val seatsInRow = scanner.nextInt()
+
+    return Cinema(rows, seatsInRow)
+}
+
+class Cinema {
+
+    var rows = 0
+    var seatsInRow = 0
+    var seats = 0
+    var r = 0
+    val m: Array<IntArray>
+
+    var lostX = 0
+    var lostY = 0
+
+    val magicNomber = 1
+
+    fun sellTicket() {
+        print("Enter a row number:\n")
+        val scanner = Scanner(System.`in`)
+        val row = scanner.nextInt()
+
+        print("Enter a seat number in that row:\n")
+        val seat = scanner.nextInt()
+
+        markPlace(row - 1, seat - 1)
+    }
+
+    private fun markPlace(row: Int, seat: Int) {
+        if (getSeat(row, seat) == 0) m[row][seat] = magicNomber
+        lostX = seat
+        lostY = row
+    }
+
+    fun getSeat(row: Int, column: Int) = m[row][column]
+
+    fun printScreenRoom() {
+        var output = "\nCinema:\n"
+        for (i in 0..rows) {
+            for (j in 0..seatsInRow) {
+                if (i == 0) {
+                    if (j == 0) output += "  "
+                    else output += "$j "
+                    continue
+                }
+                if (j == 0) {
+                    output += "$i "
+                    continue
+                }
+                output += when (getSeat(i-1, j-1)) {
+                    1 -> "B "
+                    else -> "S "
+                }
+            }
+            output += "\n"
+        }
+        output += "\n"
+        print(output)
+    }
+
+    fun calculatePrice() {
+        print("\nTicket price: ")
+        val price = when {
+            lostY < r -> 10
+            else -> 8
+        }
+        print("$$price\n")
+    }
+
+    constructor (y: Int, x: Int) {
+        this.seatsInRow = x
+        this.rows = y
+        this.seats = x * y
+        if (seats < 60) r = y
+        else r = rows / 2
+        m = Array(y) { IntArray(x) }
+    }
+}
+
+
+
+
+/*
+Stage 2/5: Sold!
+
 fun main() {
     println("Enter the number of rows:")
     val scanner = Scanner(System.`in`)
@@ -18,12 +117,12 @@ fun main() {
     }
     println("$$income")
 }
+*/
 
 
 
 /*
 Stage 1/5: Arrangement
-
 fun main() {
     // write your code here
     print("Cinema:\n" +
